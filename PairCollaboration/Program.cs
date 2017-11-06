109e77ac1e7e9c32b9095037faba91da24606cdd
kevingilfether <kevingilfether@gmail.com>
2017-11-06 14:05:18 -0500

    Wrote shortest FizzBuzz I could, failed a little

--- PairCollaboration/Program.cs
+++ PairCollaboration/Program.cs
@@ -25,6 +25,19 @@ namespace PairCollaboration
                 }
 
             }
+
+
+
+            //Broken FizzBuzz
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0)
+                    Console.Write("Fizz");
+                if (i % 5 == 0)
+                    Console.Write("Buzz");
+                else
+                    Console.Write(i);
+            }
         }
     }
 }
